f8b192cce3f00253995077803d34d9ae96e2f686
srushtimane27 <srushtimane31@gmail.com>
2022-08-05 23:25:06 +0530

    Welcome to address book system

--- src/main/java/com/CSV/ReadIntoCsv.java
+++ src/main/java/com/CSV/ReadIntoCsv.java
@@ -1,2 +1,35 @@
-package com.CSV;public class ReadIntoCsv {
+package com.CSV;
+import com.opencsv.CSVReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+/**
+ * UC14: Ablity to read the address book with person contact as CSV file
+ */
+
+public class ReadIntoCsv {
+    public static void main(String[] args) throws IOException {
+
+        //Read into CSV file
+        try {
+            Reader reader = Files.newBufferedReader(Paths.get("C:/Users/SRUSHTI/Documents/AddressBook.csv"));
+            CSVReader csvReader = new CSVReader(reader);
+            String[] records = new String[5];
+            while ((records = csvReader.readNext()) != null) {
+                System.out.println("First Name:" + records[0]);
+                System.out.println("Last Name:" + records[1]);
+                System.out.println("Address:" + records[2]);
+                System.out.println("City" + records[3]);
+                System.out.println("State" + records[4]);
+                System.out.println("Zip" + records[5]);
+                System.out.println("Phone Number" + records[6]);
+                System.out.println("Email" + records[7]);
+            }
+        } catch (Exception e) {
+            System.out.println(e.fillInStackTrace());
+        }
+
+    }
 }
--- src/main/java/com/CSV/WriteIntoCsv.java
+++ src/main/java/com/CSV/WriteIntoCsv.java
@@ -1,2 +1,27 @@
-package com.CSV;public class WriteIntoCsv {
+package com.CSV;
+
+import java.io.FileWriter;
+import com.opencsv.CSVWriter;
+
+/**
+ * UC14: Ablity to write the address book with person contact as CSV file
+ */
+
+public class WriteIntoCsv {
+    public static void main(String args[]) throws Exception {
+        //Instantiating the CSVWriter class
+        CSVWriter writer = new CSVWriter(new FileWriter("C:/Users/SRUSHTI/Documents/AddressBook.csv"));
+        //Writing data to a csv file
+        String line1[] = {"First Name", "Last Name", "Address", "City", "State", "Zip", "Phone Number", "Email"};
+        String line2[] = {"Srushti", "Mane", "Dapodi", "Pune", "Maharashtra", "42516", "451278525", "abc@gmail.com"};
+        String line3[] = {"Rasika", "Mule", "Aundh", "Satara", "Maharashtra", "42513", "236548625", "bhg@gmail.com"};
+        //Writing data to the csv file
+        writer.writeNext(line1);
+        writer.writeNext(line2);
+        writer.writeNext(line3);
+        //Flushing data from writer to file
+        writer.flush();
+        System.out.println("Data entered");
+    }
+
 }
